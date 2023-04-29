10e0ba675be4bd3e73e0102466fee0fa63633d99
RajrishiMitra <rajrishi.mitra.fiem.csds20@teamfuture.in>
2023-04-30 04:22:23 +0530

    Logical & Conditional operators

--- MAI.py
+++ MAI.py
@@ -14,7 +14,7 @@ LETTERS = string.ascii_letters
 LETTERS_DIGITS = LETTERS + DIGITS
 
 # ****************************************
-# ERROR
+# ERRORS
 # ****************************************
 
 
@@ -26,10 +26,9 @@ class Error:
         self.details = details
 
     def as_string(self):
-        result = f'{self.error_name}: {self.details}'
-        result += '\n'
-        result += f'File {self.pos_start.fn}, Line {self.pos_start.ln + 1}'
-        result += f'\n\n' + \
+        result = f'{self.error_name}: {self.details}\n'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+        result += '\n\n' + \
             string_with_arrows(self.pos_start.ftxt,
                                self.pos_start, self.pos_end)
         return result
@@ -37,7 +36,12 @@ class Error:
 
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Baka... Illegal character \n Note: ', details)
+        super().__init__(pos_start, pos_end, 'Baka... Illegal character \nNote: ', details)
+
+
+class ExpectedCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Baka... Expected character \nNote: ', details)
 
 
 class InvalidSyntaxError(Error):
@@ -70,10 +74,10 @@ class RTError(Error):
 
         return 'Traceback (most recent call last):\n' + result
 
-
 # ****************************************
 # POSITION
 # ****************************************
+
 
 class Position:
     def __init__(self, idx, ln, col, fn, ftxt):
@@ -89,7 +93,8 @@ class Position:
 
         if curr_char == '\n':
             self.ln += 1
-            self.col += 1
+            self.col = 0
+
         return self
 
     def copy(self):
@@ -106,16 +111,25 @@ MAI_ADD = 'ADD'
 MAI_SUB = 'SUB'
 MAI_MULT = 'MULT'
 MAI_DIV = 'DIV'
-MAI_LAPREN = 'LPAREN'
-MAI_RAPREN = 'RPAREN'
+MAI_LPAREN = 'LPAREN'
+MAI_RPAREN = 'RPAREN'
 MAI_EOF = 'EOF'
 MAI_POW = 'POW'
 MAI_IDENTIFIER = 'IDENTIFIER'
 MAI_KEYWORD = 'KEYWORD'
 MAI_EQ = 'EQ'
+MAI_EE = 'EE'
+MAI_NE = 'NE'
+MAI_LT = 'LT'
+MAI_GT = 'GT'
+MAI_LTE = 'LTE'
+MAI_GTE = 'GTE'
 
 KEYWORDS = [
-    'MAI'
+    'MAI',
+    'AND',
+    'OR',
+    'NOT',
 ]
 
 
@@ -123,20 +137,22 @@ class Token:
     def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
+
         if pos_start:
             self.pos_start = pos_start.copy()
             self.pos_end = pos_start.copy()
             self.pos_end.advance()
+
         if pos_end:
             self.pos_end = pos_end.copy()
 
-    def __repr__(self):
-        if self.value:
-            return f"{self.type}:{self.value}"
-        return f"{self.type}"
-
     def matches(self, type_, value):
         return self.type == type_ and self.value == value
+
+    def __repr__(self):
+        if self.value:
+            return f'{self.type}:{self.value}'
+        return f'{self.type}'
 
 # ****************************************
 # LEXER
@@ -156,8 +172,9 @@ class Lexer:
         self.curr_char = self.text[self.pos.idx] if self.pos.idx < len(
             self.text) else None
 
-    def make_token(self):
+    def make_tokens(self):
         token = []
+
         while self.curr_char != None:
             if self.curr_char in ' \t':
                 self.advance()
@@ -180,20 +197,29 @@ class Lexer:
             elif self.curr_char == '^':
                 token.append(Token(MAI_POW, pos_start=self.pos))
                 self.advance()
-            elif self.curr_char == '=':
-                token.append(Token(MAI_EQ, pos_start=self.pos))
-                self.advance()
             elif self.curr_char == '(':
-                token.append(Token(MAI_LAPREN, pos_start=self.pos))
+                token.append(Token(MAI_LPAREN, pos_start=self.pos))
                 self.advance()
             elif self.curr_char == ')':
-                token.append(Token(MAI_RAPREN, pos_start=self.pos))
+                token.append(Token(MAI_RPAREN, pos_start=self.pos))
                 self.advance()
+            elif self.curr_char == '!':
+                token1, error = self.make_not_equals()
+                if error:
+                    return [], error
+                token.append(token1)
+            elif self.curr_char == '=':
+                token.append(self.make_equals())
+            elif self.curr_char == '<':
+                token.append(self.make_less_than())
+            elif self.curr_char == '>':
+                token.append(self.make_greater_than())
             else:
                 pos_start = self.pos.copy()
                 char = self.curr_char
                 self.advance()
                 return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+
         token.append(Token(MAI_EOF, pos_start=self.pos))
         return token, None
 
@@ -207,9 +233,7 @@ class Lexer:
                 if dot_count == 1:
                     break
                 dot_count += 1
-                num_str += '.'
-            else:
-                num_str += self.curr_char
+            num_str += self.curr_char
             self.advance()
 
         if dot_count == 0:
@@ -228,6 +252,49 @@ class Lexer:
         tok_type = MAI_KEYWORD if id_str in KEYWORDS else MAI_IDENTIFIER
         return Token(tok_type, id_str, pos_start, self.pos)
 
+    def make_not_equals(self):
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.curr_char == '=':
+            self.advance()
+            return Token(MAI_NE, pos_start=pos_start, pos_end=self.pos), None
+
+        self.advance()
+        return None, ExpectedCharError(pos_start, self.pos, "'=' (after '!')")
+
+    def make_equals(self):
+        tok_type = MAI_EQ
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.curr_char == '=':
+            self.advance()
+            tok_type = MAI_EE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_less_than(self):
+        tok_type = MAI_LT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.curr_char == '=':
+            self.advance()
+            tok_type = MAI_LTE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_greater_than(self):
+        tok_type = MAI_GT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.curr_char == '=':
+            self.advance()
+            tok_type = MAI_GTE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
 
 # ****************************************
 # NODES
@@ -237,6 +304,7 @@ class Lexer:
 class NumberNode:
     def __init__(self, tok):
         self.tok = tok
+
         self.pos_start = self.tok.pos_start
         self.pos_end = self.tok.pos_end
 
@@ -247,6 +315,7 @@ class NumberNode:
 class VarAccessNode:
     def __init__(self, var_name_tok):
         self.var_name_tok = var_name_tok
+
         self.pos_start = self.var_name_tok.pos_start
         self.pos_end = self.var_name_tok.pos_end
 
@@ -255,7 +324,8 @@ class VarAssignNode:
     def __init__(self, var_name_tok, value_node):
         self.var_name_tok = var_name_tok
         self.value_node = value_node
-        self.pos_start = self.value_node.pos_start
+
+        self.pos_start = self.var_name_tok.pos_start
         self.pos_end = self.value_node.pos_end
 
 
@@ -318,43 +388,23 @@ class ParseResult:
 
 
 class Parser:
-    def __init__(self, tokens):
-        self.tokens = tokens
+    def __init__(self, token):
+        self.token = token
         self.tok_idx = -1
         self.advance()
 
-    def advance(self):
+    def advance(self, ):
         self.tok_idx += 1
-        if self.tok_idx < len(self.tokens):
-            self.curr_tok = self.tokens[self.tok_idx]
+        if self.tok_idx < len(self.token):
+            self.curr_tok = self.token[self.tok_idx]
         return self.curr_tok
-
-    def bin_op(self, func1, ops, func2=None):
-        if func2 == None:
-            func2 = func1
-        res = ParseResult()
-        left = res.register(func1())
-
-        if res.error:
-            return res
-
-        while self.curr_tok.type in ops:
-            op_tok = self.curr_tok
-            res.register_advance()
-            self.advance()
-            right = res.register(func2())
-
-            if res.error:
-                return res
-            left = BinOpNode(left, op_tok, right)
-        return res.success(left)
 
     def parse(self):
         res = self.expr()
         if not res.error and self.curr_tok.type != MAI_EOF:
             return res.failure(InvalidSyntaxError(
                 self.curr_tok.pos_start, self.curr_tok.pos_end,
-                "Expected '+' '-' '*' '/'"
+                "Expected '+', '-', '*', '/', '^', '==', '!=', '<', '>', <=', '>=', 'AND' or 'OR'"
             ))
         return res
 
@@ -372,14 +422,13 @@ class Parser:
             self.advance()
             return res.success(VarAccessNode(tok))
 
-        elif tok.type == MAI_LAPREN:
+        elif tok.type == MAI_LPAREN:
             res.register_advance()
             self.advance()
             expr = res.register(self.expr())
             if res.error:
                 return res
-
-            if self.curr_tok.type == MAI_RAPREN:
+            if self.curr_tok.type == MAI_RPAREN:
                 res.register_advance()
                 self.advance()
                 return res.success(expr)
@@ -391,7 +440,7 @@ class Parser:
 
         return res.failure(InvalidSyntaxError(
             tok.pos_start, tok.pos_end,
-            "Expected 'INT', 'FLOAT', 'IDENTIFIER', '+', '-' or '('"
+            "Expected int, float, identifier, '+', '-', '('"
         ))
 
     def power(self):
@@ -414,8 +463,36 @@ class Parser:
     def term(self):
         return self.bin_op(self.factor, (MAI_MULT, MAI_DIV))
 
+    def arith_expr(self):
+        return self.bin_op(self.term, (MAI_ADD, MAI_SUB))
+
+    def comp_expr(self):
+        res = ParseResult()
+
+        if self.curr_tok.matches(MAI_KEYWORD, 'NOT'):
+            op_tok = self.curr_tok
+            res.register_advance()
+            self.advance()
+
+            node = res.register(self.comp_expr())
+            if res.error:
+                return res
+            return res.success(UnaryOpNode(op_tok, node))
+
+        node = res.register(self.bin_op(
+            self.arith_expr, (MAI_EE, MAI_NE, MAI_LT, MAI_GT, MAI_LTE, MAI_GTE)))
+
+        if res.error:
+            return res.failure(InvalidSyntaxError(
+                self.curr_tok.pos_start, self.curr_tok.pos_end,
+                "Expected int, float, identifier, '+', '-', '(' or 'NOT'"
+            ))
+
+        return res.success(node)
+
     def expr(self):
         res = ParseResult()
+
         if self.curr_tok.matches(MAI_KEYWORD, 'MAI'):
             res.register_advance()
             self.advance()
@@ -423,8 +500,9 @@ class Parser:
             if self.curr_tok.type != MAI_IDENTIFIER:
                 return res.failure(InvalidSyntaxError(
                     self.curr_tok.pos_start, self.curr_tok.pos_end,
-                    "Expected IDENTIFIER"
+                    "Expected identifier"
                 ))
+
             var_name = self.curr_tok
             res.register_advance()
             self.advance()
@@ -434,6 +512,7 @@ class Parser:
                     self.curr_tok.pos_start, self.curr_tok.pos_end,
                     "Expected '='"
                 ))
+
             res.register_advance()
             self.advance()
             expr = res.register(self.expr())
@@ -441,14 +520,36 @@ class Parser:
                 return res
             return res.success(VarAssignNode(var_name, expr))
 
-        node = res.register(self.bin_op(self.term, (MAI_ADD, MAI_SUB)))
+        node = res.register(self.bin_op(
+            self.comp_expr, ((MAI_KEYWORD, 'AND'), (MAI_KEYWORD, 'OR'))))
 
         if res.error:
             return res.failure(InvalidSyntaxError(
                 self.curr_tok.pos_start, self.curr_tok.pos_end,
-                "Expected 'MAI', INT, FLOAT, IDENTIFIER, '+', '-' or '('"
+                "Expected 'MAI', int, float, identifier, '+', '-', '(' or 'NOT'"
             ))
+
         return res.success(node)
+
+    def bin_op(self, func_a, ops, func_b=None):
+        if func_b == None:
+            func_b = func_a
+
+        res = ParseResult()
+        left = res.register(func_a())
+        if res.error:
+            return res
+
+        while self.curr_tok.type in ops or (self.curr_tok.type, self.curr_tok.value) in ops:
+            op_tok = self.curr_tok
+            res.register_advance()
+            self.advance()
+            right = res.register(func_b())
+            if res.error:
+                return res
+            left = BinOpNode(left, op_tok, right)
+
+        return res.success(left)
 
 # ****************************************
 # RUNTIME RESULT
@@ -472,7 +573,6 @@ class RTResult:
     def failure(self, error):
         self.error = error
         return self
-
 
 # ****************************************
 # VALUES
@@ -520,6 +620,41 @@ class Number:
     def power_by(self, other):
         if isinstance(other, Number):
             return Number(self.value ** other.value).set_context(self.context), None
+
+    def get_comparison_eq(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value == other.value)).set_context(self.context), None
+
+    def get_comparison_ne(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value != other.value)).set_context(self.context), None
+
+    def get_comparison_lt(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value < other.value)).set_context(self.context), None
+
+    def get_comparison_gt(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value > other.value)).set_context(self.context), None
+
+    def get_comparison_lte(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value <= other.value)).set_context(self.context), None
+
+    def get_comparison_gte(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value >= other.value)).set_context(self.context), None
+
+    def anded_by(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value and other.value)).set_context(self.context), None
+
+    def ored_by(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value or other.value)).set_context(self.context), None
+
+    def notted(self):
+        return Number(1 if self.value == 0 else 0).set_context(self.context), None
 
     def copy(self):
         copy = Number(self.value)
@@ -576,10 +711,15 @@ class Interpreter:
         return method(node, context)
 
     def no_visit_method(self, node, context):
-        raise Exception(f'No visit {type(node).__name__} method defined')
+        raise Exception(f'No visit_{type(node).__name__} method defined')
+
+    ###################################
 
     def visit_NumberNode(self, node, context):
-        return RTResult().success(Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end))
+        return RTResult().success(
+            Number(node.tok.value).set_context(
+                context).set_pos(node.pos_start, node.pos_end)
+        )
 
     def visit_VarAccessNode(self, node, context):
         res = RTResult()
@@ -592,6 +732,7 @@ class Interpreter:
                 f"'{var_name}' is not defined",
                 context
             ))
+
         value = value.copy().set_pos(node.pos_start, node.pos_end)
         return res.success(value)
 
@@ -601,6 +742,7 @@ class Interpreter:
         value = res.register(self.visit(node.value_node, context))
         if res.error:
             return res
+
         context.symbol_table.set(var_name, value)
         return res.success(value)
 
@@ -623,6 +765,22 @@ class Interpreter:
             result, error = left.divided_by(right)
         elif node.op_tok.type == MAI_POW:
             result, error = left.power_by(right)
+        elif node.op_tok.type == MAI_EE:
+            result, error = left.get_comparison_eq(right)
+        elif node.op_tok.type == MAI_NE:
+            result, error = left.get_comparison_ne(right)
+        elif node.op_tok.type == MAI_LT:
+            result, error = left.get_comparison_lt(right)
+        elif node.op_tok.type == MAI_GT:
+            result, error = left.get_comparison_gt(right)
+        elif node.op_tok.type == MAI_LTE:
+            result, error = left.get_comparison_lte(right)
+        elif node.op_tok.type == MAI_GTE:
+            result, error = left.get_comparison_gte(right)
+        elif node.op_tok.matches(MAI_KEYWORD, 'AND'):
+            result, error = left.anded_by(right)
+        elif node.op_tok.matches(MAI_KEYWORD, 'OR'):
+            result, error = left.ored_by(right)
 
         if error:
             return res.failure(error)
@@ -634,9 +792,13 @@ class Interpreter:
         number = res.register(self.visit(node.node, context))
         if res.error:
             return res
+
         error = None
+
         if node.op_tok.type == MAI_SUB:
-            number = number.multiply_by(Number(-1))
+            number, error = number.multiply_by(Number(-1))
+        elif node.op_tok.matches(MAI_KEYWORD, 'NOT'):
+            number, error = number.notted()
 
         if error:
             return res.failure(error)
@@ -650,23 +812,24 @@ class Interpreter:
 
 global_symbol_table = SymbolTable()
 global_symbol_table.set("NULL", Number(0))
+global_symbol_table.set("FALSE", Number(0))
+global_symbol_table.set("TRUE", Number(1))
 
 
 def run(fn, text):
-    # Token generation
+    # Generate token
     lexer = Lexer(fn, text)
-    tokens, error = lexer.make_token()
-
+    token, error = lexer.make_tokens()
     if error:
         return None, error
 
-    # Syntax Tree generation
-    parser = Parser(tokens)
+    # Generate AST
+    parser = Parser(token)
     ast = parser.parse()
     if ast.error:
         return None, ast.error
 
-    # Run Program
+    # Run program
     interpreter = Interpreter()
     context = Context('<program>')
     context.symbol_table = global_symbol_table
